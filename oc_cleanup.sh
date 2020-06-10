e7a1193e2f4e9f9abba9aa66284f8b0ec062e9f3
Gowrishankar Muthukrishnan <gmuthukr@redhat.com>
2020-06-10 14:42:03 +0530

    infrared cleanup for openstack

---
+++ oc_cleanup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -x
+echo "virsh list --all"
+virsh list --all
+
+virsh destroy undercloud-0
+virsh destroy controller-0
+virsh undefine undercloud-0
+virsh undefine controller-0
+rm -f /var/lib/libvirt/images/undercloud-0*
+rm -f /var/lib/libvirt/images/controller-0*
+
+echo "removed kvm guests"
+
+echo "virsh list --all"
+virsh list --all
+
+echo "virsh net-list --all"
+virsh net-list --all
+
+virsh net-destroy br-ctlplane
+virsh net-destroy br-link0
+virsh net-destroy br-link1
+virsh net-destroy management
+virsh net-undefine br-ctlplane
+virsh net-undefine br-link0
+virsh net-undefine br-link1
+virsh net-undefine management
+ifconfig br-ctlplane down
+ifconfig br-link0 down
+ifconfig br-link1 down
+ifconfig management down
+brctl delbr br-ctlplane
+brctl delbr br-link0
+brctl delbr br-link1
+brctl delbr management
+rm -f /etc/sysconfig/network-scripts/ifcfg-br-ctlplane
+rm -f /etc/sysconfig/network-scripts/ifcfg-br-link0
+rm -f /etc/sysconfig/network-scripts/ifcfg-br-link1
+echo "removed kvm networks"
+
+echo "virsh net-list --all"
+virsh net-list --all
